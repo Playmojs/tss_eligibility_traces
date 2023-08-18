ec7ab53f8c2c3d4d6fdb03bac399a769a68305b3
Simen Storesund <simen.st@hotmail.com>
2023-08-18 23:58:20 +0200

    Fix gif-creation and save nice test

--- eligibility_navigation.py
+++ eligibility_navigation.py
@@ -22,8 +22,10 @@ def scheduleFrame(event_series, time):
 def addTrace(symbols, ids):
     for id in ids:
         if symbols[id].activated:
+            if not symbols[id].tag:
+                print("New symbol tagged")
             symbols[id].tag = True
-            symbols[id].spike_delay_ms *= 0.7
+            symbols[id].spike_delay_ms *= 0.9
 
 def catchFrame(symbols, time, recorder):
     plot_data = np.empty((2,0))
@@ -32,33 +34,37 @@ def catchFrame(symbols, time, recorder):
             continue 
         if time-symbol.activated_at < 10:
             plot_data = np.hstack((plot_data, np.reshape(symbol.coord,(2,1))))
-    frame = plt.scatter(plot_data[:][0], plot_data[:][1])
-    plt.xlim(0,10)
-    plt.ylim(0,10)
-    plt.show()
+    # plt.scatter(plot_data[:][0], plot_data[:][1])
+    # plt.xlim(0,10)
+    # plt.ylim(0,10)
+    # plt.show()
     recorder.plots.append(plot_data)
 
 
 def eligibilityNavigation(symbols, start, goal, distance, ms_per_frame):
-    valid_transitions = utils.findAllTransitions(symbols,distance)
+    valid_transitions = utils.findAllTransitions(symbols,distance) #Get a list containing, for each symbol, all their valid transitions
     current_time = 0
     goal_found = False
     event_series = {}
     recorder = rec.Recorder()
 
-    for frame in range(0,1000, ms_per_frame):
-        scheduleFrame(event_series, frame)
-
+    #reset
+    for symbol in symbols:
+        symbol.tag = False
+        symbol.spike_delay_ms = 10
+    
     symbols[goal].tag = True
     
-    for i in range(5):
+    for i in range(10):
+        for frame in range(0,100, ms_per_frame):
+            scheduleFrame(event_series, current_time + frame)
 
         for symbol in symbols:
             symbol.activated = False
             symbol.activated_at = None
 
         symbols[start].activated = True
-        symbols[start].activated_at = 0
+        symbols[start].activated_at = current_time
         goal_found = False
 
         scheduleActivate(event_series, current_time + symbols[start].spike_delay_ms, valid_transitions[start].transition_ids)
@@ -72,6 +78,7 @@ def eligibilityNavigation(symbols, start, goal, distance, ms_per_frame):
                 if activate_id == goal:
                     goal_found = True
                 if symbols[activate_id].tag:
+                    print("Add tag")
                     addTrace(symbols, valid_transitions[activate_id].transition_ids)
                 symbols[activate_id].activated = True
                 symbols[activate_id].activated_at = current_time
@@ -84,13 +91,20 @@ def eligibilityNavigation(symbols, start, goal, distance, ms_per_frame):
 
 
 
-            if current_time > 100:
+            if current_time > 1000:
                 raise Exception("Too much time passed, out of bounds")
             del event_series[current_time]
-        print("The goal was found after", current_time, "ms!")
+        final_time = current_time
+        print("The goal was found after", final_time, "ms!")
+        while final_time + 15 > current_time:
+            current_time = min(event_series)
+            if event_series[current_time].catch_frame:
+                catchFrame(symbols, current_time, recorder)
+            del event_series[current_time]
+
         event_series.clear()
 
-    print(recorder.plots)
+    print(len(recorder.plots))
     recorder.createAnimation()
     return
 
@@ -109,22 +123,6 @@ print ("at ", symbols[start].coord, "and", symbols[goal].coord)
 
 # %%
 
-eligibilityNavigation(symbols, start, goal, 2, 5)
-# %%
-fig, ax = plt.subplots()
-rng = np.random.default_rng(19680801)
-data = np.array([20, 20, 20, 20])
-x = np.array([1, 2, 3, 4])
+eligibilityNavigation(symbols, start, goal, 2, 1)
 
-artists = []
-colors = ['tab:blue', 'tab:red', 'tab:green', 'tab:purple']
-for i in range(20):
-    data += rng.integers(low=0, high=10, size=data.shape)
-    container = ax.barh(x, data, color=colors)
-    artists.append(container)
-
-
-ani = animation.ArtistAnimation(fig=fig, artists=artists, interval=400)
-plt.show()
-ani.save("test.gif")
 # %%
--- recorder.py
+++ recorder.py
@@ -7,10 +7,13 @@ class Recorder:
     def createAnimation(self):
         fig = plt.figure()
         ax = plt.axes(xlim = (0,10), ylim = (0,10))
-        scat = ax.scatter(self.plots[0][:][0], self.plots[0][:][1])
 
         def update(frame):
-            scat.set_offsets(self.plots[frame])
+            ax.clear()
+            ax.scatter(self.plots[frame][:][0],self.plots[frame][:][1])
+            ax.set_xlim(0,10)
+            ax.set_ylim(0,10)
 
         ani = animation.FuncAnimation(fig, func = update, frames = len(self.plots), interval = 50)
         plt.show()
+        ani.save("test.gif")
