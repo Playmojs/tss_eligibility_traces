3bd23a44c49fac33088c0fbc92ab0dd00e650134
Simen Storesund <simen.st@hotmail.com>
2023-12-14 11:34:43 +0100

    Update sim-runner for cluster

--- grid_simulation/Simulation_runner.py
+++ grid_simulation/Simulation_runner.py
@@ -1,8 +1,8 @@
 from LIF_theta_model3 import *
 
 output_files = "data/december/ThM3_6000s_"
-n_simulations = 3
+n_simulations = 1
 
 for i in range(n_simulations):
     print(f"Starting simulation {i + 1} out of {n_simulations}")
-    gridSimulation(48, 13, 0.1, 3, False, False, 1000000, False, True, 1000, output_files + str(i))
+    gridSimulation(48, 13, 0.1, 6000, False, False, 1000000, False, True, 1000, output_files + str(i))
