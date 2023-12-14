796cd6ca34601b961533a78e19b8c67aa3652394
Simen Storesund <simen.st@hotmail.com>
2023-12-14 11:35:16 +0100

    update sim-runner for cluster

--- grid_simulation/Simulation_runner.py
+++ grid_simulation/Simulation_runner.py
@@ -5,4 +5,4 @@ n_simulations = 1
 
 for i in range(n_simulations):
     print(f"Starting simulation {i + 1} out of {n_simulations}")
-    gridSimulation(48, 13, 0.1, 6000, False, False, 1000000, False, True, 1000, output_files + str(i))
+    gridSimulation(48, 13, 0.1, 6000, False, False, 1000000, False, True, 10000, output_files + str(i))
